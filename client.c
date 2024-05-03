c3ad56320b3a299d9ce12bb2e63ad1bce22053c6
Andrew Finley <andrew.j.finley-1@ou.edu>
2024-05-02 19:50:00 -0500

    encryption init

--- client.c
+++ client.c
@@ -3,38 +3,78 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 #define PORT 8080
+#define AES_KEY_SIZE 128
+
+void handle_error() {
+    ERR_print_errors_fp(stderr);
+    exit(EXIT_FAILURE);
+}
 
 int main() {
     struct sockaddr_in address;
-    int sock = 0, valread;
-    struct sockaddr_in serv_addr;
+    int sock = 0;
     char *hello = "Hello from client";
     char buffer[1024] = {0};
 
+    SSL_CTX *ctx;
+    SSL *ssl;
+
+    // Initialize the SSL library
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+    ctx = SSL_CTX_new(TLS_client_method());
+
+    // Create a new SSL structure for a connection
+    ssl = SSL_new(ctx);
+
+    // Creating socket file descriptor
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
         return -1;
     }
 
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    memset(&address, '0', sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+    if(inet_pton(AF_INET, "127.0.0.1", &address.sin_addr) <= 0) {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&address, sizeof(address)) < 0) {
         printf("\nConnection Failed \n");
         return -1;
     }
-    send(sock, hello, strlen(hello), 0);
+
+    // Set up SSL connection
+    SSL_set_fd(ssl, sock);
+    if (SSL_connect(ssl) <= 0) {
+        handle_error();
+    }
+
+    // Send message to the server
+    SSL_write(ssl, hello, strlen(hello));
     printf("Hello message sent\n");
-    valread = read(sock, buffer, 1024);
+
+    // Receive response from the server
+    int valread;
+    if ((valread = SSL_read(ssl, buffer, 1024)) < 0) {
+        handle_error();
+    }
     printf("%s\n", buffer);
+
+    // Clean up
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+    close(sock);
     return 0;
 }
--- server.c
+++ server.c
@@ -3,15 +3,44 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 #define PORT 8080
+#define AES_KEY_SIZE 128
+
+void handle_error() {
+    ERR_print_errors_fp(stderr);
+    exit(EXIT_FAILURE);
+}
 
 int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
-    const char *hello = "Hello from server";
+    char *hello = "Hello from server";
+
+    SSL_CTX *ctx;
+    SSL *ssl;
+
+    // Initialize the SSL library
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+    ctx = SSL_CTX_new(TLS_server_method());
+
+    // Load server certificate and key
+    if (SSL_CTX_use_certificate_file(ctx, "server.crt", SSL_FILETYPE_PEM) <= 0) {
+        handle_error();
+    }
+
+    if (SSL_CTX_use_PrivateKey_file(ctx, "server.key", SSL_FILETYPE_PEM) <= 0 ) {
+        handle_error();
+    }
+
+    // Create a new SSL structure for a connection
+    ssl = SSL_new(ctx);
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -36,13 +65,26 @@ int main() {
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    int valread;
-    if ((valread = read(new_socket, buffer, 1024)) < 0) {
-        perror("read");
-        exit(EXIT_FAILURE);
+
+    // Set up SSL connection
+    SSL_set_fd(ssl, new_socket);
+    if (SSL_accept(ssl) <= 0) {
+        handle_error();
     }
-    printf("%s\n",buffer);
-    send(new_socket, hello, strlen(hello), 0);
+
+    int valread;
+    if ((valread = SSL_read(ssl, buffer, 1024)) < 0) {
+        handle_error();
+    }
+    printf("%s\n", buffer);
+
+    SSL_write(ssl, hello, strlen(hello));
     printf("Hello message sent\n");
+
+    // Clean up
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+    close(server_fd);
     return 0;
 }
