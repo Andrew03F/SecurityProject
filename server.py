e6886e0889580c90b9dbe0c79ea3cba89f457b16
David Lu <david07.lu@gmail.com>
2024-05-02 20:27:02 -0500

    add encryption

--- server.py
+++ server.py
@@ -1,7 +1,45 @@
 import socket
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import os
+
+# Function to derive key from password using PBKDF2
+def derive_key(password, salt):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,  # 256-bit key (AES-256)
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    return kdf.derive(password)
+
+# Function to encrypt data using AES
+def encrypt(plaintext, key):
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(plaintext) + padder.finalize()
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+    return iv + ciphertext
+
+# Function to decrypt ciphertext using AES
+def decrypt(ciphertext, key):
+    iv = ciphertext[:16]
+    ciphertext = ciphertext[16:]
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    padded_data = decryptor.update(ciphertext) + decryptor.finalize()
+    unpadder = padding.PKCS7(128).unpadder()
+    plaintext = unpadder.update(padded_data) + unpadder.finalize()
+    return plaintext
 
 def main():
-    HOST = '127.0.0.1'
+    HOST = '10.204.167.105'
     PORT = 8080
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((HOST, PORT))
@@ -10,13 +48,21 @@ def main():
     conn, addr = server_socket.accept()
     print('Connected to', addr)
     
+    salt = os.urandom(16)
+    conn.sendall(salt)  # Send salt to client
+    
+    password = input("Enter password for encryption/decryption: ").encode()
+    key = derive_key(password, salt)
+    
     while True:
         data = conn.recv(1024)
         if not data:
             break
-        print("Received message:", data.decode())
+        decrypted_data = decrypt(data, key)
+        print("Received message:", decrypted_data.decode())
         reply = input("Enter your reply: ")
-        conn.sendall(reply.encode())
+        encrypted_reply = encrypt(reply.encode(), key)
+        conn.sendall(encrypted_reply)
     
     conn.close()
 
